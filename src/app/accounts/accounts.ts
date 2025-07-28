836b554c326a48f35fd750b7a2642992e8d8c40b
delmas007 <azertyazerty1ze@gmail.com>
2025-07-28 09:58:05 +0000

    dockerisation

--- src/app/accounts/accounts.ts
+++ src/app/accounts/accounts.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-accounts',
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './accounts.html',
   styleUrl: './accounts.css'
 })
-export class AccountsComponent {
+export class AccountsComponent implements OnInit{
+  accounts: any;
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit() {
+    this.http.get("http://localhost:8888/ACCOUNT-SERVICE/accounts")
+      .subscribe({
+        next : data => {
+          this.accounts = data;
+        },
+        error : err => {
+          console.log(err);
+        }
+      })
+  }
 
 }
+
